d1b7209c3ce7c186c8268a44ce0b92f2837ad281
Noah <noghspace@hotmail.com>
2019-09-14 20:58:29 +0800

    Update some files...

---
+++ demo/little-app/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+        {{msg}}
+        <div>
+            <input type="text" v-model="info">
+            <button @click="handleClick">Add</button>
+            <ul>
+                <!-- <li v-for="item in list">{{item}}</li> -->
+                <!-- <todo-item v-for="item in list" :key="item" :item="item"></todo-item> -->
+                <todo-item v-for="item in list" :key="item" :item="item">
+                  <!-- 2.5 引用插槽写法 - 具名插槽 -->
+                  <!-- <span slot="todoItem" style="font-size: 20px">{{item}}</span> -->
+                  <!-- 2.6 引用插槽写法 - 具名插槽 -->
+                  <template v-slot:todoItem>
+                    <span style="font-size: 20px">{{item}}</span>                    
+                  </template>
+                </todo-item>
+            </ul>
+        </div>
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  data() {
+    return {
+        msg: 'Hello Vue!',
+        info: '',
+        list: [],
+    }
+  },
+  methods: {
+    handleClick() {
+      var strLength = this.info.replace(/ /g, "").length;
+      // alert(strLength);
+      // alert(this.info);
+      if(typeof this.info == "undefined" || this.info == "" || this.info == null || strLength == 0) {
+          alert("信息：" + this.info);
+      } else {
+          console.log(this.info);
+          this.list.push(this.info);
+          this.info = '';
+      }
+    }
+  },
+  components: {
+    HelloWorld,
+    TodoItem
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ demo/little-app/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="item">
+    <input type="checkbox" v-model="checked">
+    <!-- 插槽写法 - 具名插槽 -->
+    <slot name="todoItem"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  data() {
+    return {
+      checked: false,
+    }
+  }
+}
+</script>
+
+<style>
+  .item {
+    color: blueviolet;
+  }
+</style>
